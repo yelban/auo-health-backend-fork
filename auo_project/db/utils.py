ea9fb23e0612f405c360e8a3d55be0d58253454a
Luffy Tsai <luffy.tsai@deepinsight.tw>
2023-04-08 15:53:44 +0800

    [fix] pytest init flow

--- auo_project/db/utils.py
+++ auo_project/db/utils.py
@@ -4,13 +4,12 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from auo_project.core.config import settings
 
 
-def get_url():
-    # return  make_url(str(f"postgresql+asyncpg://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}:{settings.DATABASE_PORT}"))
-    return settings.ASYNC_DATABASE_URI
+def get_url(db_name="template1"):
+    return f"postgresql+asyncpg://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{db_name}"
 
 
-def get_engine():
-    db_url = get_url()
+def get_engine(dbname):
+    db_url = get_url(dbname)
     conn_args = {}
     if settings.DATABASE_SSL_REQURED:
         conn_args["ssl"] = "require"
@@ -22,16 +21,19 @@ def get_engine():
     return engine
 
 
-async def create_database(engine=None) -> None:
-    """Create a databse."""
-    dbname = (
-        f"{settings.DATABASE_NAME}_testing"
-        if settings.ENVIRONMENT == "testing"
-        else settings.DATABASE_NAME
-    )
+async def drop_conn(dbname, engine):
+    async with engine.connect() as conn:
+        await conn.execute(
+            text(
+                f"SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '{dbname}' AND pid <> pg_backend_pid();",  # noqa: E501, S608
+            ),
+        )
 
+
+async def create_database(dbname, engine=None) -> None:
+    """Create a databse."""
     if not engine:
-        engine = get_engine()
+        engine = get_engine("template1")
 
     async with engine.connect() as conn:
         database_existance = await conn.execute(
@@ -42,33 +44,43 @@ async def create_database(engine=None) -> None:
         database_exists = database_existance.scalar() == 1
 
     if database_exists:
-        await drop_database()
+        await drop_conn(dbname, engine)
+        await drop_database(dbname)
 
-    async with engine.connect() as conn:  # noqa: WPS440
+    postgres_engine = get_engine("postgres")
+    await drop_conn("template1", postgres_engine)
+    async with postgres_engine.connect() as conn:  # noqa: WPS440
         await conn.execute(
             text(
                 f'CREATE DATABASE "{dbname}" ENCODING "utf8" TEMPLATE template1;',  # noqa: E501
             ),
         )
 
-    engine = get_engine()
+    engine = get_engine(dbname)
     async with engine.connect() as conn:  # noqa: WPS440
         await conn.execute(
             text(
                 f'CREATE SCHEMA IF NOT EXISTS "app"',  # noqa: E501
             ),
         )
+        await conn.execute(
+            text(
+                f'CREATE SCHEMA IF NOT EXISTS "measure"',  # noqa: E501
+            ),
+        )
+        await conn.commit()
+        schemas = await conn.execute(
+            text(
+                "select schema_name from information_schema.schemata;",
+            ),
+        )
+        print(schemas.fetchall())
 
 
-async def drop_database(engine=None) -> None:
+async def drop_database(dbname, engine=None) -> None:
     """Drop current database."""
-    dbname = (
-        f"{settings.DATABASE_NAME}_testing"
-        if settings.ENVIRONMENT == "testing"
-        else settings.DATABASE_NAME
-    )
     if not engine:
-        engine = get_engine()
+        engine = get_engine("template1")
     async with engine.connect() as conn:
         disc_users = (
             "SELECT pg_terminate_backend(pg_stat_activity.pid) "  # noqa: S608
@@ -77,8 +89,4 @@ async def drop_database(engine=None) -> None:
             "AND pid <> pg_backend_pid();"
         )
         await conn.execute(text(disc_users))
-        await conn.execute(text(f'DROP DATABASE "{dbname}"'))
-
-
-def load_all_models():
-    pass
+        await conn.execute(text(f'DROP DATABASE IF EXISTS "{dbname}"'))
--- auo_project/tests/conftest.py
+++ auo_project/tests/conftest.py
@@ -14,7 +14,7 @@ from sqlalchemy.orm import sessionmaker
 from auo_project.core.config import settings
 from auo_project.db.dependencies import get_db_session
 from auo_project.db.init_db import init_db
-from auo_project.db.utils import create_database, drop_database
+from auo_project.db.utils import create_database, drop_database, get_engine
 from auo_project.services.redis.dependency import get_redis_pool
 from auo_project.tests.utils import AuthTestClient
 from auo_project.web.api.deps import get_db
@@ -23,14 +23,6 @@ from auo_project.web.application import get_app
 ENVIRONMENT = "testing"
 os.environ["ENVIRONMENT"] = ENVIRONMENT
 settings.ENVIRONMENT = ENVIRONMENT
-
-
-# @pytest.fixture(scope="session")
-# def event_loop():
-#     policy = asyncio.get_event_loop_policy()
-#     loop = policy.new_event_loop()
-#     yield loop
-#     loop.close()
 
 
 @pytest.fixture(scope="session")
@@ -60,7 +52,7 @@ async def _engine() -> AsyncGenerator[AsyncEngine, None]:
     :yield: new engine.
     """
     from auo_project.db.meta import meta  # noqa: WPS433
-    from auo_project.db.models import load_all_models  # noqa: WPS433
+    from auo_project.models import load_all_models  # noqa: WPS433
 
     load_all_models()
 
@@ -71,17 +63,12 @@ async def _engine() -> AsyncGenerator[AsyncEngine, None]:
     )
     from auo_project.db.session import engine
 
-    # engine = create_async_engine(
-    #     f"postgresql+asyncpg://auo_project:auo_project@auo_project-db:5432/{dbname}",
-    #     echo=True,
-    #     future=True,
-    # )
-
+    engine = get_engine(dbname)
     try:
         yield engine
     finally:
         await engine.dispose()
-        await drop_database()
+        await drop_database(dbname)
 
 
 @pytest.fixture
@@ -102,7 +89,6 @@ async def db_session(
 
     session_maker = sessionmaker(
         bind=connection,
-        # bind=engine,
         autocommit=False,
         autoflush=False,
         expire_on_commit=False,
@@ -169,45 +155,37 @@ async def client(
 
 
 @pytest.fixture(autouse=True)
-async def initialize_db(
-    db_session: AsyncSession,
-) -> AsyncGenerator[None, None]:
+async def initialize_db() -> AsyncGenerator[None, None]:
     """
     Create models and databases.
     :yield: new engine.
     """
     from auo_project.db.meta import meta  # noqa: WPS433
-    from auo_project.db.models import load_all_models  # noqa: WPS433
+    from auo_project.models import load_all_models  # noqa: WPS433
 
-    # from auo_project.models import Action
     load_all_models()
-
-    await create_database()
-
-    # print("settings", settings)
 
     dbname = (
         f"{settings.DATABASE_NAME}_testing"
         if settings.ENVIRONMENT == "testing"
         else settings.DATABASE_NAME
     )
-    print("dbname", dbname)
-    # engine = create_async_engine(
-    #     f"postgresql+asyncpg://auo_project:auo_project@auo_project-db:5432/{dbname}",
-    # )
-    from auo_project.db.session import engine
+    await create_database(dbname)
 
-    # engine = create_async_engine(
-    #     f"postgresql+asyncpg://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{dbname}",
-    # )
-    # print("engine", engine)
+    engine = get_engine(dbname)
     async with engine.begin() as conn:
         await conn.run_sync(meta.create_all)
-    print("created all")
-    await init_db(db_session=db_session)
-    await db_session.commit()
-    print("commit before yield")
+        local_session = sessionmaker(
+            bind=conn,
+            autocommit=False,
+            autoflush=False,
+            expire_on_commit=False,
+            class_=AsyncSession,
+        )
+        await init_db(db_session=local_session())
+        await conn.commit()
+
     yield
 
     await engine.dispose()
-    await drop_database()
+    await drop_database(dbname)
