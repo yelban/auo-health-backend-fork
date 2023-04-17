ad379e6ccf07c00b9fae8cf581a894aafde6b3c1
Luffy Tsai <luffy.tsai@deepinsight.tw>
2023-04-17 22:34:52 +0800

    [feat] add new api to get upload file info

--- auo_project/web/api/v1/endpoints/file.py
+++ auo_project/web/api/v1/endpoints/file.py
@@ -11,6 +11,24 @@ from auo_project.web.api import deps
 router = APIRouter()
 
 
+@router.get("/{file_id}")
+async def get_upload_file(
+    file_id: str,
+    *,
+    db_session: AsyncSession = Depends(deps.get_db),
+    current_user: models.User = Depends(deps.get_current_active_user),
+    ip_allowed: bool = Depends(deps.get_ip_allowed),
+) -> Any:
+    """
+    Get upload file.
+    """
+    file = await crud.file.get(db_session=db_session, id=file_id)
+    if not file:
+        raise HTTPException(status_code=404, detail=f"Not found file id: {file_id}")
+
+    return file
+
+
 @router.post("/cancel-upload/{file_id}")
 async def cancel_upload_file(
     file_id: str,
