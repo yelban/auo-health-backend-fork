98cccde7c07a20a37452e3bf818966ba1fee43fa
Luffy Tsai <luffy.tsai@deepinsight.tw>
2023-09-18 23:43:54 +0800

    [feat] update worker number

--- auo_project/worker-start.sh
+++ auo_project/worker-start.sh
@@ -4,4 +4,4 @@ set -e
 # TODO
 # python /app/app/celeryworker_pre_start.py
 
-celery -A auo_project.services.celery.tasks worker -l info -Q main-queue --autoscale=10,2
+celery -A auo_project.services.celery.tasks worker -l info -Q main-queue --autoscale=10,5
