78625c9443a56dec74f85634b761d38485236a81
Luffy Tsai <luffy.tsai@deepinsight.tw>
2024-03-31 13:48:58 +0800

    [feat] update

---
+++ migration/20240225.sql
@@ -0,0 +1,15 @@
+INSERT INTO "measure"."question_options"("category_id","value","label","component","memo","created_at","updated_at")
+VALUES
+(E'c057',E'007',E'配偶',NULL,NULL,now(),now()),
+(E'c057',E'008',E'子女',NULL,NULL,now(),now()),
+(E'c057',E'009',E'父母/公婆/岳父母',NULL,NULL,now(),now()),
+(E'c057',E'010',E'(外)祖父母',NULL,NULL,now(),now()),
+(E'c057',E'011',E'(外)孫子女',NULL,NULL,now(),now()),
+(E'c057',E'012',E'兄弟姊妹(或配偶的兄弟姊妹)',NULL,NULL,now(),now()),
+(E'c057',E'013',E'同學/朋友',NULL,NULL,now(),now())
+;
+
+
+INSERT INTO "measure"."surveys"("org_id","name","description","created_at","updated_at")
+VALUES
+(E'39317568-ebd7-4cf4-a390-6934e4c95843',E'內測問卷2',E'STANDARD_wZN8X_中醫診斷現代化研究問卷',NOW(),NOW());
